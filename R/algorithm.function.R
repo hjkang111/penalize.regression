99941022f22016e47bb2720065ee9640e44ba651
hjkang111 <naomiggjoohyun@gmail.com>
2025-05-18 15:33:43 +0900

    algorithm functions

--- R/algorithm.function.R
+++ R/algorithm.function.R
@@ -1,14 +1,84 @@
-#다른 파일에 있는 penalty_function 불러와서 이용할 것임
+# algorithm functions
 
-#options(repos = c(CRAN = "https://cran.r-project.org"))
+# soft threshold function
+soft_threshold <- function(z, lambda) {
+  return(sign(z) * pmax(0, abs(z) - lambda))
+}
 
-#install.packages("devtools")
+# CGD
+perform_CGD <- function(X, y, method, lambda, learning_rate, max_iter) {
+  n <- nrow(X)
+  p <- ncol(X)
+  beta <- rep(0, p)
 
-# devtools로 GitHub에서 'penalize.regression' 패키지 설치
-#devtools::install_github("hjkang111/penalize.regression")
-#devtools::install()
-#library(penalize.regression)
+  for (iter in 1:max_iter) {
+    for (j in 1:p) {
+      pred <- X %*% beta
+      residual <- pred - y
+      grad_j <- (2/n) * sum(X[, j] * residual) + grad_penalty(beta[j], method, lambda)
 
+      beta[j] <- beta[j] - learning_rate * grad_j
+    }
+  }
+
+  return(beta)
+}
+
+# PGD
+perform_PGD <- function(X, y, lambda, learning_rate, max_iter) {
+  n <- nrow(X)
+  p <- ncol(X)
+  beta <- rep(0, p)
+
+  for (iter in 1:max_iter) {
+    pred <- X %*% beta
+    grad <- (2/n) * t(X) %*% (pred - y)
+
+    beta <- soft_threshold(beta - learning_rate * grad, learning_rate * lambda)
+  }
+
+  return(beta)
+}
+
+# FISTA
+perform_FISTA <- function(X, y, lambda, learning_rate, max_iter) {
+  n <- nrow(X)
+  p <- ncol(X)
+  beta <- rep(0, p)
+  z <- beta
+  t_k <- 1
+
+  for (iter in 1:max_iter) {
+    pred <- X %*% z
+    grad <- (2/n) * t(X) %*% (pred - y)
+
+    beta_new <- soft_threshold(z - learning_rate * grad, learning_rate * lambda)
+    t_k_new <- (1 + sqrt(1 + 4 * t_k^2)) / 2
+    z <- beta_new + ((t_k - 1) / t_k_new) * (beta_new - beta)
+
+    beta <- beta_new
+    t_k <- t_k_new
+  }
+
+  return(beta)
+}
+
+# Newton
+perform_Newton <- function(X, y, lambda, max_iter) {
+  n <- nrow(X)
+  p <- ncol(X)
+  beta <- rep(0, p)
+
+  for (iter in 1:max_iter) {
+    pred <- X %*% beta
+    grad <- (2/n) * t(X) %*% (pred - y)
+    hess <- (2/n) * t(X) %*% X + diag(rep(1e-6, p))  # 정칙화 추가
+
+    beta <- beta - solve(hess) %*% grad
+  }
+
+  return(as.vector(beta))
+}
 
 
 # gradient descent
@@ -29,30 +99,6 @@ perform_GD <- function(X, y, method, lambda, learning_rate, max_iter) {
     gradient <- (2/n) * t(X) %*% residual + penalty_function(beta, method, lambda)
 
     beta <- beta - learning_rate * gradient
-  }
-
-  return(beta)
-}
-
-#CDA 함수부터 수정하기
-
-# coordinate descent
-perform_CDA <- function(X, y, method, lambda, max_iter) {
-  n <- nrow(X)
-  p <- ncol(X)
-
-  beta <- rep(0, p)
-
-  # 반복문을 통해 Coordinate Descent 최적화
-  for (iter in 1:max_iter) {
-    for (j in 1:p) {
-      # 예측값 계산
-      pred <- X %*% beta
-      residual <- y - pred + X[, j] * beta[j]
-
-      # 새로운 계수 계산 (Soft Thresholding)
-      beta[j] <- sign(t(X[, j]) %*% residual) * max(0, abs(t(X[, j]) %*% residual) - lambda)
-    }
   }
 
   return(beta)
